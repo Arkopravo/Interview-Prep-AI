5130ffab94ea414069f44ddce6a530370b3d5d39
Arkopravo <arkopravoghosh789@gmail.com>
2025-08-28 23:18:05 +0530

    18:43

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+//TO DO:  Add Hero img here
+
+import React, { useState } from 'react'
+import HERO_IMG from "../assets/hero-img.jpg"
+import { useNavigate } from 'react-router-dom'
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
+
+  const handleCTA = () => {
+
+  };
+
   return (
     <div>LandingPage</div>
   )
