d96693b917ed94fccc1392872afed26dd8c7baef
Arkopravo <arkopravoghosh789@gmail.com>
2025-08-31 17:26:40 +0530

    38:01

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Toaster } from "react-hot-toast";
-import Login from "./pages/Auth/Login";
 import LandingPage from './pages/LandingPage';
-import SignUp from './pages/Auth/SignUp';
 import Dashboard from './pages/Home/Dashboard';
 import InterviewPrep from './pages/InterviewPrep/InterviewPrep';
   
@@ -15,8 +13,6 @@ const App = () => {
           {/* Default routes */}
           <Route path='/' element={<LandingPage/>} />
 
-          <Route path='/login' element={<Login/>} />
-          <Route path='/signUp' element={<SignUp/>} />
           <Route path='/dashboard' element={<Dashboard/>} />
           <Route path='/interview-prep/:sessionId' element={<InterviewPrep/>} />
         </Routes>
