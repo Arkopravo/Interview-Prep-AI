fd2e1d960a6cd73de1bc1ba57bc1ceb91e3f86e5
Arkopravo <arkopravoghosh789@gmail.com>
2025-09-14 00:15:45 +0530

    2:37:06

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -6,6 +6,9 @@ import { LuCircleAlert, LuListCollapse } from 'react-icons/lu'
 import SpinnerLoader from '../../components/Loader/SpinnerLoader'
 import {toast} from "react-hot-toast"
 import DashboardLayout from '../../components/layouts/DashboardLayout'
+import RoleInfoHeader from './components/RoleInfoHeader'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
 
 const InterviewPrep = () => {
 
@@ -20,7 +23,19 @@ const InterviewPrep = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isUpdateLoader, setIsUpdateLoader] = useState(false);
 
-  const fetchSessionDetailsById = async () => {};
+  const fetchSessionDetailsById = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.SESSION.GET_ONE(sessionId)
+      );
+
+      if(response.data && response.data.session) {
+        setSessionData(response.data.session);
+      }
+    } catch(error) {
+      console.error("Error: ", error);
+    }
+  };
 
   const generateConceptExplanation = async () => {};
 
@@ -48,6 +63,24 @@ const InterviewPrep = () => {
           sessionData?.updatedAt ? moment(sessionData.updatedAt).format("Do MMM YYYY") : ""
         }
       />
+
+      <div className=''>
+        <h2 className=''>Interview Q & A</h2>
+      </div>
+ 
+      <div className=''>
+        <div className={`col-span-12 ${
+          openLearnMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+        }`}>
+          <AnimatePresence>
+            {sessionData?.questions?.map((data, index) => {
+              return (
+                <motion.dev
+              )
+            })}
+          </AnimatePresence>
+        </div>
+      </div>
     </DashboardLayout>
   )
 }
