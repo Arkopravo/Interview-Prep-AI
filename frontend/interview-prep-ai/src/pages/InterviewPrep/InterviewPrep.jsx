39d4403042c122b20b700f3c7d43757edd8c6811
Arkopravo <arkopravoghosh789@gmail.com>
2025-09-14 16:04:56 +0530

    2:56:23

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import moment from 'moment'
-import {AnimatePresence, motion} from "framer-motion"
-import { LuCircleAlert, LuListCollapse } from 'react-icons/lu'
-import SpinnerLoader from '../../components/Loader/SpinnerLoader'
-import {toast} from "react-hot-toast"
-import DashboardLayout from '../../components/layouts/DashboardLayout'
-import RoleInfoHeader from './components/RoleInfoHeader'
-import axiosInstance from '../../utils/axiosInstance'
-import { API_PATHS } from '../../utils/apiPaths'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import moment from "moment";
+import { AnimatePresence, motion } from "framer-motion";
+import { LuCircleAlert, LuListCollapse } from "react-icons/lu";
+import SpinnerLoader from "../../components/Loader/SpinnerLoader";
+import { toast } from "react-hot-toast";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import RoleInfoHeader from "./components/RoleInfoHeader";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import QuestionCard from "../../components/Cards/QuestionCard";
 
 const InterviewPrep = () => {
+  const { sessionId } = useParams();
 
-  const {sessionId} = useParams();
-  
   const [sessionData, setSessionData] = useState(null);
   const [errorMsg, setErrorMsg] = useState("");
 
@@ -29,28 +29,39 @@ const InterviewPrep = () => {
         API_PATHS.SESSION.GET_ONE(sessionId)
       );
 
-      if(response.data && response.data.session) {
+      if (response.data && response.data.session) {
         setSessionData(response.data.session);
       }
-    } catch(error) {
+    } catch (error) {
       console.error("Error: ", error);
     }
   };
 
   const generateConceptExplanation = async () => {};
 
-  const toggleQuestionPinStatus = async (questionId) => {};
+  const toggleQuestionPinStatus = async (questionId) => {
+    try {
+      const response = await axiosInstance.post(
+        API_PATHS.QUESTION.PIN(questionId)
+      );
+      if (response.data && response.data.question) {
+        fetchSessionDetailsById();
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   const uploadMoreQuestions = async () => {};
 
   useEffect(() => {
-    if(sessionId) {
+    if (sessionId) {
       fetchSessionDetailsById();
     }
 
     return () => {};
   }, []);
- 
+
   return (
     <DashboardLayout>
       <RoleInfoHeader
@@ -60,29 +71,73 @@ const InterviewPrep = () => {
         questions={sessionData?.questions?.length || "-"}
         description={sessionData?.description || ""}
         lastUpdated={
-          sessionData?.updatedAt ? moment(sessionData.updatedAt).format("Do MMM YYYY") : ""
+          sessionData?.updatedAt
+            ? moment(sessionData.updatedAt).format("Do MMM YYYY")
+            : ""
         }
       />
 
-      <div className=''>
-        <h2 className=''>Interview Q & A</h2>
-      </div>
- 
-      <div className=''>
-        <div className={`col-span-12 ${
-          openLearnMoreDrawer ? "md:col-span-7" : "md:col-span-8"
-        }`}>
-          <AnimatePresence>
-            {sessionData?.questions?.map((data, index) => {
-              return (
-                <motion.dev
-              )
-            })}
-          </AnimatePresence>
+      <div className="container mx-auto pt-4 pb-4 px-4 md:px-0">
+        <h2 className="text-lg font-semibold color-black">Interview Q & A</h2>
+
+        <div className="grid grid-cols-12 gap-4 mt-5 mb-10">
+          <div
+            className={`col-span-12 ${
+              openLearnMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+            }`}
+          >
+            <AnimatePresence>
+              {sessionData?.questions?.map((data, index) => {
+                return (
+                  <motion.dev
+                    key={data._id || index}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, scale: 0.95 }}
+                    transition={{
+                      duration: 0.4,
+                      type: "spring",
+                      stiffness: 100,
+                      delay: index * 0.1,
+                      damping: 15,
+                    }}
+                    layout
+                    layoutId={`question-${data._id || index}`}
+                  >
+                    <>
+                      <QuestionCard
+                        question={data?.question}
+                        answer={data?.answer}
+                        openLearnMore={() =>
+                          generateConceptExplanation(data.question)
+                        }
+                        isPinned={data?.isPinned}
+                        onTogglePin={() => toggleQuestionPinStatus(data._id)}
+                      />
+                    </>
+                  </motion.dev>
+                );
+              })}
+            </AnimatePresence>
+          </div>
         </div>
       </div>
+      
+      <div>
+        <Drawer
+          isOpen={openLearnMoreDrawer}
+          onClose={() => setOpenLearnMoreDrawer(false)}
+          title={!isLoading && explanation?.title}
+        >
+          {errorMsg && (
+            <p className="">
+              <LuCircleAlert className="" /> {errorMsg}
+            </p>
+          )}
+        </Drawer>
+      </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default InterviewPrep
+export default InterviewPrep;
